951f19ca11564ec3d61f03a1ea3bb4e89b429ac3
Zach Baxter <zbax777@gmail.com>
2016-07-23 15:03:52 -0500

    add admin field

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160611163150) do
+ActiveRecord::Schema.define(version: 20160723200150) do
 
   create_table "employees", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "username"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "admin",      default: false
   end
 
   create_table "time_cards", force: :cascade do |t|
